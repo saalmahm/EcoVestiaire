9b16eb8691ff37adf5d38b136d6339a2cbec082f
salma hamdi <saalmahm51@gmail.com>
2026-01-18 10:34:49 +0100

    feat: initialize EcoVestiaire Spring Boot backend (Maven, PostgreSQL, base structure)

---
+++ src/main/java/ma/ecovestiaire/backend/EcoVestiaireApplication.java
@@ -0,0 +1,13 @@
+package ma.ecovestiaire.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoVestiaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoVestiaireApplication.class, args);
+	}
+
+}
