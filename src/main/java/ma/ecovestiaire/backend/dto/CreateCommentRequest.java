f2d33b606d25ed29afbf5afc0760bd001fa58f56
salma hamdi <saalmahm51@gmail.com>
2026-01-23 20:04:18 +0100

    Ajouter le DTO CreateCommentRequest

---
+++ src/main/java/ma/ecovestiaire/backend/dto/CreateCommentRequest.java
@@ -0,0 +1,11 @@
+package ma.ecovestiaire.backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CreateCommentRequest {
+
+    @NotBlank
+    private String content;
+}
