7668f6bae938d219a518ed163fcf99a837f76f36
salma hamdi <saalmahm51@gmail.com>
2026-01-23 20:04:40 +0100

    Ajouter CommentController pour supprimer un commentaire

---
+++ src/main/java/ma/ecovestiaire/backend/controller/CommentController.java
@@ -0,0 +1,22 @@
+package ma.ecovestiaire.backend.controller;
+
+import ma.ecovestiaire.backend.service.CommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable("id") Long commentId) {
+        commentService.deleteComment(commentId);
+        return ResponseEntity.noContent().build();
+    }
+}
