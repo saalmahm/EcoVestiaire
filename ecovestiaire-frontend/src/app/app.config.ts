6aa66bb5def63b4e018afbf8edb12980109026a9
salma hamdi <saalmahm51@gmail.com>
2026-02-13 15:30:16 +0100

    feat(frontend) : mise en place de l'architecture core, du service API et de l'intercepteur JWT

--- ecovestiaire-frontend/src/app/app.config.ts
+++ ecovestiaire-frontend/src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { environment } from './environments/environment';
+import { API_BASE_URL } from './core/tokens/api-base-url.token';
+import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
+    { provide: API_BASE_URL, useValue: environment.apiBaseUrl },
+    provideAnimationsAsync()
+  ]
 };
